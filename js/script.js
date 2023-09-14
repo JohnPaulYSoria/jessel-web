3a422997225bd487658040b4e1b64b2112353303
John Paul Soria <142276915+JohnPaulYSoria@users.noreply.github.com>
2023-09-14 09:02:44 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function showAlert1() {
+    var myText = "Mini Bagpack Added in your Cart !";
+    alert (myText);
+  }
+
+function showAlert2() {
+    var myText = "Gray Shoulder Bag Added in your Cart !";
+    alert (myText);
+  }
+
+function showAlert3() {
+    var myText = "Stripe Shoulder Bag Added in your Cart !";
+    alert (myText);
+  }
+
+function showAlert4() {
+    var myText = "Blue Shoulder Bag Added in your Cart !";
+    alert (myText);
+  }
+
+function showAlert5() {
+    var myText = "Bagpacks Added in your Cart !";
+    alert (myText);
+  }
+
+function showAlert6() {
+    var myText = "Red Velvet Bag Added in your Cart !";
+    alert (myText);
+  }
